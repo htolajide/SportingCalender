b9a6c9c0180048594828d11570a81c8f15db2dfe
Hammed Taofeek <htolajide@yahoo.com>
2026-03-25 17:31:31 +0100

    Initial project setup

---
+++ SportingCalender/src/main/java/com/sportrader/SportingCalender/SportingCalenderApplication.java
@@ -0,0 +1,13 @@
+package com.sportrader.SportingCalender;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SportingCalenderApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SportingCalenderApplication.class, args);
+	}
+
+}
---
+++ SportingCalender/src/test/java/com/sportrader/SportingCalender/SportingCalenderApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sportrader.SportingCalender;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SportingCalenderApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
