3054708ba53df8021d118b0bed5ebafbfc08f68c
Hammed Taofeek <htolajide@yahoo.com>
2026-03-26 13:23:58 +0100

    Event controller created

---
+++ src/main/java/com/sportrader/SportingCalender/controller/EventController.java
@@ -0,0 +1,41 @@
+package com.sportrader.SportingCalender.controller;
+
+import com.sportradar.calendar.entity.Event;
+import com.sportradar.calendar.service.EventService;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/events")
+@CrossOrigin(origins = "*") // Allow frontend access
+public class EventController {
+
+    private final EventService eventService;
+
+    public EventController(EventService eventService) {
+        this.eventService = eventService;
+    }
+
+    @GetMapping
+    public ResponseEntity<List<Event>> getAllEvents() {
+        return ResponseEntity.ok(eventService.getAllEvents());
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Event> getEventById(@PathVariable Long id) {
+        try {
+            Event event = eventService.getEventById(id);
+            return ResponseEntity.ok(event);
+        } catch (RuntimeException e) {
+            return ResponseEntity.notFound().build();
+        }
+    }
+
+    @PostMapping
+    public ResponseEntity<Event> createEvent(@RequestBody Event event) {
+        Event savedEvent = eventService.createEvent(event);
+        return ResponseEntity.status(201).body(savedEvent);
+    }
+}
