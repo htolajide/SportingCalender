74f32dcc1f4f9c69f22a7b516b763be3b889f714
Hammed Taofeek <htolajide@yahoo.com>
2026-03-29 17:00:17 +0100

    github workflow action added

--- src/main/java/com/sportrader/SportingCalender/repository/EventRepository.java
+++ src/main/java/com/sportrader/SportingCalender/repository/EventRepository.java
@@ -4,7 +4,6 @@ import com.sportrader.SportingCalender.entity.Event;
 import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import java.time.LocalDate;
